596b1e701a85ddf79a18d8a14f50743110d81368
Ígor Haag <igorhaagrod@gmail.com>
2023-04-24 17:39:42 -0300

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "pink": {
+          "50": "#f5ecf4",
+          "100": "#ebd9e8",
+          "200": "#d6b3d2",
+          "300": "#c28cbb",
+          "400": "#ad66a5",
+          "500": "#99408e",
+          "600": "#7a3372",
+          "700": "#5c2655",
+          "800": "#3d1a39",
+          "900": "#1f0d1c"
+        }
+      }
     },
   },
   plugins: [],
