ede9e7bcbdf5e40409a6b07faac4aeab91949f1a
Ígor Haag <igorhaagrod@gmail.com>
2023-08-03 17:06:58 -0300

    feat: scripts for google analytics

---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,24 @@
+import Script from 'next/script';
+
+export function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        id='google-analytics-script'
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id='google-analytics-config' strategy='lazyOnload'>
+        {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+                });
+            `}
+      </Script>
+    </>
+  );
+}
